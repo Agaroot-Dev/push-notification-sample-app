07c7f10b615831c9a48a76b35af3417699766ee4
kondo0602 <kondo8363@gmail.com>
2025-02-27 16:32:05 +0900

    feature: トークン取得処理を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // 通知に関する設定を取得する
+  //
+  // この記述があるとアプリの起動時に「...は通知を送信します。よろしいですか？」というダイアログが表示される
+  // provisional: trueが設定されている場合、初回の通知を受信してから通知を許可するか決めることができる
+  final notificationSettings =
+      await FirebaseMessaging.instance.requestPermission();
+
+  // FCMトークンを取得する
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print('FCMトークン: $fcmToken');
 
   runApp(const MyApp());
 }
